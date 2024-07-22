ada05dfe0342b9ed410a8f13b5e2161dc3b41fe9
Simon Staß <simonsta95@gmail.com>
2024-07-22 10:54:10 +0200

    Added a Service class

--- src/main/java/org/spring/asterixapi/AsterixController.java
+++ src/main/java/org/spring/asterixapi/AsterixController.java
@@ -3,17 +3,16 @@ package org.spring.asterixapi;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/asterix")
 public class AsterixController {
 
-    private final CharacterRepo characterRepo;
+    public final AsterixService asterixService;
 
-    public AsterixController(CharacterRepo characterRepo) {
-        this.characterRepo = characterRepo;
+    public AsterixController(AsterixService asterixService) {
+        this.asterixService = asterixService;
     }
 
     @GetMapping("/characters")
@@ -22,76 +21,25 @@ public class AsterixController {
                                             @RequestParam(required = false) Integer age,
                                             @RequestParam (required = false) String profession){
 
-        List<Character> characters = new ArrayList<>();
-        List<Character> allCharacters = characterRepo.findAll();
-
-        if (id != null) {
-            for (Character character : allCharacters) {
-                if (character.id().equals(id)) {
-                    characters.add(character);
-                }
-            }
-            return characters;
-        }
-
-        if (name != null) {
-            for (Character character : allCharacters) {
-                if (character.name().startsWith(name)) {
-                    characters.add(character);
-                }
-            }
-            return characters;
-        }
-
-        if (age != null) {
-            for (Character character : allCharacters) {
-                if (character.age() == age) {
-                    characters.add(character);
-                }
-            }
-            return characters;
-        }
-
-        if (profession != null) {
-            for (Character character : allCharacters) {
-                if (character.profession().equals(profession)) {
-                    characters.add(character);
-                }
-            }
-            return characters;
-        }
-
-        return allCharacters;
+        return asterixService.getCharacters(id,name, String.valueOf(age),profession);
 
     }
 
     @PostMapping("/characters")
     public String addCharacter(@RequestBody Character character) {
-        characterRepo.save(character);
-        return character.toString() + " added to Database";
+        return asterixService.addCharacter(character);
     }
+
 
     @PutMapping("/characters")
     public String updateCharacter(@RequestBody Character UpdateCharacter) {
-        List<Character> characters = characterRepo.findAll();
-        for (Character character : characters) {
-            if (character.id().equals(UpdateCharacter.id())) {
-                characterRepo.save(character);
-            }
-        }
 
-        return UpdateCharacter.toString() + " updated to " + characterRepo.findAll().toString();
+        return asterixService.updateCharacter(UpdateCharacter);
     }
 
     @DeleteMapping("/characters")
     public String deleteCharacter(@RequestBody String characterId) {
-        List<Character> characters = characterRepo.findAll();
-        for (Character character : characters) {
-            if (character.id().equals(characterId)) {
-                characterRepo.delete(character);
-            }
-        }
 
-        return characterRepo.findAll().toString() + " deleted from Database";
+        return asterixService.deleteCharacter(characterId);
     }
 }
---
+++ src/main/java/org/spring/asterixapi/AsterixService.java
@@ -0,0 +1,92 @@
+package org.spring.asterixapi;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AsterixService {
+
+    private final CharacterRepo characterRepo;
+
+    public AsterixService(CharacterRepo characterRepo){
+        this.characterRepo = characterRepo;
+    }
+
+
+    public List<Character> getCharacters(String id,
+                                         String name,
+                                         String age,
+                                         String profession){
+        List<Character> characters = new ArrayList<>();
+        List<Character> allCharacters = characterRepo.findAll();
+
+        if (id != null) {
+            for (Character character : allCharacters) {
+                if (character.id().equals(id)) {
+                    characters.add(character);
+                }
+            }
+            return characters;
+        }
+
+        if (name != null) {
+            for (Character character : allCharacters) {
+                if (character.name().startsWith(name)) {
+                    characters.add(character);
+                }
+            }
+            return characters;
+        }
+
+        if (age != null) {
+            for (Character character : allCharacters) {
+                if (character.age() == Integer.parseInt(age)) {
+                    characters.add(character);
+                }
+            }
+            return characters;
+        }
+
+        if (profession != null) {
+            for (Character character : allCharacters) {
+                if (character.profession().equals(profession)) {
+                    characters.add(character);
+                }
+            }
+            return characters;
+        }
+
+        return allCharacters;
+    }
+
+    public String addCharacter(@RequestBody Character character) {
+        characterRepo.save(character);
+        return character.toString() + " added to Database";
+    }
+
+    public String updateCharacter(@RequestBody Character UpdateCharacter) {
+        List<Character> characters = characterRepo.findAll();
+        for (Character character : characters) {
+            if (character.id().equals(UpdateCharacter.id())) {
+                characterRepo.save(character);
+            }
+        }
+
+        return UpdateCharacter.toString() + " updated to " + characterRepo.findAll().toString();
+    }
+
+    public String deleteCharacter(@RequestBody String characterId) {
+        List<Character> characters = characterRepo.findAll();
+        for (Character character : characters) {
+            if (character.id().equals(characterId)) {
+                characterRepo.delete(character);
+            }
+        }
+
+        return characterRepo.findAll().toString() + " deleted from Database";
+    }
+
+}
